8bc82daf3fc7596e47fd6b9384558efd25ce9ad3
Nicolas BUSSER <nicolas.busser67@gmail.com>
2022-03-26 12:53:49 +0100

    add basic file parsing

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const fs = require('fs');
 const api = require('./lawnmowers.js');
 
-api.set_top_right_position({x: 2, y: 2});
-const lawnmower = api.add_lawnmower({x: 0, y: 0}, 'N');
-api.move_lawnmower(lawnmower, 'N')
-api.print_all_lawnmowers_infos();
+const lines = fs.readFileSync('instructions.txt', 'utf-8').split('\n');
+
+const top_right_pos = lines[0].split(' ');
+api.set_top_right_position({x: top_right_pos[0], y: top_right_pos[1]});
+
+for (let i = 1; i < lines.length; i += 2) {
+    const lawnmower_pos = lines[i].split(' ');
+    api.add_lawnmower({
+        x: lawnmower_pos[0],
+        y: lawnmower_pos[1],
+    }, lawnmower_pos[2]);
+
+    const lawnmower_path = [...lines[i+1]];
+    lawnmower_path.forEach(direction => {
+        console.log(direction);
+    });
+}
